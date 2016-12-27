832dd9a70c9eb2b96248a1aa7c7bafdbe0d3673b
ricky <ricky3andika@gmail.com>
2016-12-27 08:11:44 +0700

    Tambah fungsi untuk skenario behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,12 +3,16 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use app\models\Skp;
+use app\models\SkpItem;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    private $skp;
+
     /**
      * Initializes context.
      *
@@ -18,5 +22,26 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        $this->skp = new Skp();
     }
+
+    /**
+    * @When I add the skpItem to the skp with nama :nama
+    */
+    public function addSkpItemToSkp($nama)
+    {
+        $this->skp->addSkpItems(new SkpItem($nama));
+    }
+
+    /**
+    * @Then the number of skpitem should be :skpItemCount
+    */
+    public function theSkpItemCountShouldBe($skpItemCount)
+    {
+        PHPUnit_Framework_Assert::assertSame(
+            count($this->skp->getSkpItems()),
+            intval($skpItemCount)
+        );
+    }
+
 }
